74d71b78a6a71b4028c94f6c946f3d305fb26820
yaheng <yaheng.su1@gmail.com>
2017-02-21 12:14:44 -0700

    set up for edit workouts and edit exercises activities, tweaking around with file inputs and outputs

--- app/src/main/java/com/example/xtrem/werkit/EditWorkoutActivity.java
+++ app/src/main/java/com/example/xtrem/werkit/EditWorkoutActivity.java
@@ -1,18 +1,25 @@
 package com.example.xtrem.werkit;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class EditWorkoutActivity extends AppCompatActivity {
     public static final String FILENAME_WORKOUTS = "workouts";
     public static final String FILENAME_EXERCISES = "exercises";
     public static final String FILENAME_HISTORY = "history";
+    public String string = "test";
+    File file = new File()
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_workout);
+        FileOutputStream fos = getApplicationContext().openFileOutput(FILENAME_EXERCISES, Context.MODE_APPEND);
     }
 
 
